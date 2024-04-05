af7d8987e82007cb4f5d2ad625589be462d4d407
Dario de souza <darioo_dario@hotmail.com>
2024-04-05 11:19:04 -0300

    menu button added

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import CustomLink from './link'
 import { AlignJustify } from 'lucide-react'
+import { MenuButton } from './menu-button'
 
 const NavBarHeader = () => {
   return (
@@ -9,7 +10,9 @@ const NavBarHeader = () => {
       <div className='h-16 max-w-screen-lg mx-auto px-3 flex justify-between items-center'>
         <a href="">LOgo</a>
         <nav className="">
-          <AlignJustify className='sm:hidden' />
+          <MenuButton>
+            <AlignJustify />
+          </MenuButton>
           <div className="hidden space-x-3 sm:flex" >
             <CustomLink href="/" className="block p-4 hover:bg-slate-300 transition-all duration-500">Home</CustomLink>
             <CustomLink href="/" className="block p-4 hover:bg-slate-300 transition-all duration-500">Contato</CustomLink>
