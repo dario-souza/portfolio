912d42d824fa9a63b0d4ab37db7888c7eed27821
Dario de souza <darioo_dario@hotmail.com>
2024-04-23 19:56:53 -0300

    style: ajuste footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='fixed bottom-0 w-full p-8 border-t'>
+    <footer className='fixed bottom-0 w-full p-8 border-t backdrop-blur-md'>
       <span className='block text-xs text-zinc-400 text-center'>Desenvolvido com ❤️ por Dário</span>
     </footer>
   )
