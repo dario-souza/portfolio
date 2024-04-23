0731db03dbb6f56162e6f858b852dd407451c142
Dario de souza <darioo_dario@hotmail.com>
2024-04-23 19:10:27 -0300

    chore:mudança do idioma da página di inglês para pt-br

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="pt-BR" suppressHydrationWarning>
       <body className={cn(inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <HeaderComponent />
