3bb3afbb8429d92c44dc35bd43cca03f1253d67d
Dario de souza <darioo_dario@hotmail.com>
2024-04-27 18:57:36 -0300

    feature: efeito máquina de escrever com lib 'typewriter'

--- src/components/home-page-presentation.tsx
+++ src/components/home-page-presentation.tsx
@@ -1,21 +1,15 @@
 'use client'
-import { typerWriter } from '@/app/utils/type-writer';
-import React, { useRef } from 'react'
+import { Typewriter } from 'react-simple-typewriter'
+import React from 'react'
 
 
 const HomePagePresentation = () => {
-  let paragraph = useRef<HTMLParagraphElement>(null);
-
-  React.useEffect(() => {
-    if (paragraph.current) {
-      typerWriter(paragraph.current.innerText, paragraph.current)
-    }
-  }, [])
-
+  const text = Typewriter({ words: ['Web Developer', 'Typescripiteiro', ''], loop: true, cursor: true })
   return (
     <div>
       <h1 className="text-4xl mb-2 sm:text-5xl md:text-6xl lg:text-8xl">Dario de souza</h1>
-      <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center text-anime" ref={paragraph} >I am <strong>Web developer</strong></p>
+      <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center text-anime" >I am <strong>{text}</strong></p>
+
     </div>
   )
 }
