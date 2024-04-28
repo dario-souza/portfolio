182b18100d1135022dd50338c78f94be55bdb721
Dario de souza <darioo_dario@hotmail.com>
2024-04-28 14:53:24 -0300

    chore: container inserido na pagina home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
 import { Metadata } from "next";
 import HomePagePresentation from "@/components/home-page-presentation";
+import Container from "@/components/container";
+
 
 
 export const metadata: Metadata = {
@@ -8,9 +10,10 @@ export const metadata: Metadata = {
 }
 export default function Home() {
   return (
-    <main className="flex justify-center items-center max-w-screen-lg mx-auto px-3 h-[calc(100vh-145px)]">
-      <HomePagePresentation />
+    <main>
+      <Container>
+        <HomePagePresentation />
+      </Container>
     </main>
-
   );
 }
