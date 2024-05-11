23dfa32cf6a177d476718b32f7963eb5c127e6ee
Dario de souza <darioo_dario@hotmail.com>
2024-05-10 21:45:27 -0300

    style: responsividade button

--- src/components/portfolio-card.tsx
+++ src/components/portfolio-card.tsx
@@ -1,15 +1,20 @@
+'use client'
 import React from 'react'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
 import Image from 'next/image'
 import { Button } from './ui/button'
 import { Portfolio } from '@/app/type/types-portfolios'
-
+import { GitHubLogoIcon, GlobeIcon } from '@radix-ui/react-icons'
+import Link from 'next/link'
+import useMedia from '@/app/hooks/use-media'
 
 type ProjectsCardsProps = {
   card: Portfolio
 }
 
 const PortfolioCards = ({ card }: ProjectsCardsProps) => {
+  const screenSmall = useMedia('(max-width: 600px)')
+
   return (
     <Card className='hover:scale-[1.02] transition-all duration-300'>
       <CardHeader>
@@ -25,7 +30,21 @@ const PortfolioCards = ({ card }: ProjectsCardsProps) => {
         <p className='hidden sm:block'>{card.description}</p>
       </CardContent>
       <CardFooter>
-        <Button variant="destructive">Saiba mais</Button>
+        {screenSmall ? <Button variant='destructive' size='sm' >Saiba mais</Button> : <Button variant='destructive' >Saiba mais</Button>}
+        <Link href={card.repo} className='ml-3' target='_blank'>
+          <GitHubLogoIcon
+            width={25}
+            height={25}
+            className='hover:text-[#918FF3] transition-all duration-500'
+          />
+        </Link>
+        <Link href={card.url} className='ml-3' target='_blank'>
+          <GlobeIcon
+            width={25}
+            height={25}
+            className='hover:text-[#918FF3] transition-all duration-500'
+          />
+        </Link>
       </CardFooter>
     </Card>
   )
