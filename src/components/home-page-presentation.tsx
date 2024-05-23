8b64f54930b3508629e3201177a84f5046cb6a6c
Dario de souza <darioo_dario@hotmail.com>
2024-05-23 10:09:39 -0300

    em construção

--- src/components/home-page-presentation.tsx
+++ src/components/home-page-presentation.tsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 
 const HomePagePresentation = () => {
-  const text = Typewriter({ words: ['Web Developer', 'Em contrução...',], loop: true, cursor: true })
+  const text = Typewriter({ words: ['em construção...',], loop: true, cursor: true })
   return (
     <div>
-      <h1 className="text-4xl mb-2 sm:text-5xl md:text-6xl lg:text-8xl">Dario de souza</h1>
-      <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center text-anime" >I am <strong>{text}</strong></p>
+      <h1 className="text-4xl mb-2 sm:text-5xl md:text-6xl lg:text-8xl">Página em construção 🏗️🚧</h1>
+      <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center text-anime" ><strong>{text}</strong></p>
 
     </div>
   )
