1565ab1ed21c425beb48bdf3a81656cd2ffdbf02
Dario de souza <darioo_dario@hotmail.com>
2024-05-23 21:42:13 -0300

    chore: correção de textos na página about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 const AboutPage = () => {
   return (
     <section className='max-w-screen-lg mx-auto px-4 py-4 sm:px-8 grid grid-cols-1 sm:grid-cols-3 gap-2 sm:gap-x-8'>
-      <div>
+      <div className='mb-8'>
         <Image className='rounded-full shadow-md mx-auto' src="https://github.com/sh4kaa.png" alt='Dario-foto' width={460} height={460} sizes="(max-width: 600px) 100vw, 60vw" priority />
         <p className='mt-4 italic text-center text-xs font-thin'>Programação por paixão, dinheiro é consequência, e pagando bem, que mal tem rs</p>
       </div>
@@ -20,14 +20,14 @@ const AboutPage = () => {
         </p>
         <h1 className='text-center text-2xl font-semibold'>O interesse</h1>
         <p className='leading-6'>
-          Meu interesse pela tecnologia surgiu desde minha adolescência, meu primeiro contato com tecnologia foi no ensino fundamental, onde uma vez por semana tínhamos aula de informática, foi quando aprendi à salvar um arquivo de imagem usando o aplicativo paint, na época o sistema operacional era Windows 98 e o acesso à informática era bem restrito, nem todo mundo tinha acesso a um computador pessoal, e eu estava nessa lista de pessoas. <br /> <br /> Desde então fiquei fissurado por tecnologia, queria saber como funcionava realmente o sistema de um computador, como era possível emular jogos antigos no computador, quem desenvolvia os programas e como tudo aquilo era feito, eram questões que burbulhavam em minha mente naquela época.
+          Meu interesse pela tecnologia surgiu desde minha adolescência, meu primeiro contato com tecnologia foi no ensino fundamental, onde uma vez por semana tínhamos aula de informática, foi quando aprendi à salvar um arquivo de imagem usando o aplicativo paint, na época o sistema operacional era Windows 98 e o acesso à informática era bem restrito, nem todo mundo tinha acesso a um computador pessoal, e eu estava nessa lista de pessoas. <br /> <br /> Desde então, fiquei fissurado por tecnologia, queria saber como realmente funcionava o sistema de um computador, como era possível emular jogos antigos no computador, quem desenvolvia os programas e como tudo aquilo era feito, eram questões que burbulhavam em minha mente naquela época.
 
-          O que eu precisa era apenas de um computador, não precisa de internet e inclusive nessa época não existia internet banda larga, só internet discada, que cheguei à ter acesso graças ao meu primeiro trabalho, meu ex patrão tinha computador com internet discada, ele deixava eu usar aos finais de semana, ele também me ajudou a conquistar meu primeiro computador um &quot;AMD K6 550mhz&quot; e nessa época aprendi a fazer reparos em computador, tanto no sistema quanto na parte física montagem e manutenção.
+          O que eu precisava era apenas de um computador, não precisava de internet e inclusive nessa época não existia internet banda larga, só internet discada, que cheguei à ter acesso graças ao meu primeiro trabalho, meu ex patrão tinha computador com internet discada, ele deixava eu usar aos finais de semana, ele também me ajudou a conquistar meu primeiro computador um &quot;AMD K6 550mhz&quot; e nessa época aprendi a fazer reparos em computador, tanto no sistema quanto na parte física montagem e manutenção.
         </p>
 
         <h1 className='text-center text-2xl font-semibold'>Atualmente</h1>
         <p className='leading-6'>
-          Atualmente trabalho como como analista de suporte, reservo sempre um tempo para programar e estudar programação, participo de eventos e imersões de programação, dentro do escopo de front-end e back-end. Tenho bons conhecimentos em desenvolvimento web utilizando as tecnologias HTML Javascript e css puro, e também utilizando frameworks e bibliotecas como por exemplo, React e Nextjs, onde é possível desenvolver aplicações web escaláveis de uma forma mais moderna e robusta e de fácil manutenabilidade. Tenho conhecimento de como integrar API&apos;s no front-end, tenho um pouco de familiaridade com back-end utilizando NodeJS
+          Atualmente trabalho como como analista de suporte, reservo sempre um tempo para programar e estudar programação, participo de eventos e imersões de programação sempre alinhado com meus objetivos, dentro do escopo de front-end e back-end, mas por enquanto meu forte é mais front-end. Tenho bons conhecimentos em desenvolvimento web utilizando as tecnologias HTML Javascript e css puro, e também utilizando frameworks e bibliotecas como por exemplo, React e Nextjs, onde é possível desenvolver aplicações web escaláveis de uma forma mais moderna e robusta e de fácil manutenabilidade. Utilizo versionamento de código com git e github, tenho conhecimento de como integrar API&apos;s no front-end, tenho um pouco de familiaridade com back-end utilizando NodeJS
         </p>
 
       </div>
