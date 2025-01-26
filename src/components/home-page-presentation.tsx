634ca9d892c8d353af1f498e283963f76dcff285
Dario de souza <sh4kaa@outlook.com>
2025-01-26 19:53:00 -0300

    página em manutenção

--- src/components/home-page-presentation.tsx
+++ src/components/home-page-presentation.tsx
@@ -7,7 +7,7 @@ const HomePagePresentation = () => {
   const text = Typewriter({ words: ['em construção...',], loop: true, cursor: true })
   return (
     <div>
-      <h1 className="text-4xl mb-2 sm:text-5xl md:text-6xl lg:text-8xl">Página em construção 🏗️🚧</h1>
+      <h1 className="text-4xl mb-2 sm:text-5xl md:text-6xl lg:text-8xl">Página em manutenção🚧</h1>
       <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center text-anime" ><strong>{text}</strong></p>
 
     </div>
