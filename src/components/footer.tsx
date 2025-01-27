2c28430647bb56442f2bc6cb3d73c8de75f8df53
Dario souza <sh4kaa@outlook.com>
2025-01-27 20:01:08 -0300

    chore: alteração da data no footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='fixed bottom-0 w-full p-8 border-t backdrop-blur-md z-20'>
-      <span className='block text-xs text-zinc-400 text-center'>Desenvolvido por Dário ❤️‍🔥🚀 | Todos os direitos reservados - 2024 </span>
+      <span className='block text-xs text-zinc-400 text-center'>Desenvolvido por Dário ❤️‍🔥🚀 | Todos os direitos reservados - 2025 </span>
     </footer>
   )
 }
