ce814528e5ee7d8b21cf8988a44c3c8a4d78d157
Dario de souza <sh4kaa@outlook.com>
2025-03-09 18:15:01 -0300

    chore: adição de tech redux

--- src/components/home/section-right.tsx
+++ src/components/home/section-right.tsx
@@ -1,25 +1,42 @@
-import { FaReact } from "react-icons/fa";
-import { Card, CardContent } from "../ui/card"
-import { SiJavascript, SiStyledcomponents, SiTypescript } from "react-icons/si";
-import { RiNextjsLine } from "react-icons/ri"
-import { FaNode } from "react-icons/fa";
-import { SiMongodb } from "react-icons/si";
+import { FaReact } from 'react-icons/fa'
+import { Card, CardContent } from '../ui/card'
+import { SiJavascript, SiStyledcomponents, SiTypescript } from 'react-icons/si'
+import { RiNextjsLine } from 'react-icons/ri'
+import { FaNode } from 'react-icons/fa'
+import { SiMongodb } from 'react-icons/si'
+import { TbBrandRedux } from 'react-icons/tb'
 
 const technologies = [
-  { name: "React", icon: <FaReact className="text-blue-500" size={30} /> },
-  { name: "JavaScript", icon: <SiJavascript className="text-yellow-500" size={30} /> },
-  { name: "Next.js", icon: <RiNextjsLine className="text-gray-800" size={30} /> },
-  { name: "Styled-Components", icon: <SiStyledcomponents className="text-pink-500" size={30} /> },
-  { name: "Node.js", icon: <FaNode className="text-green-500" size={30} /> },
-  { name: "TypeScript", icon: <SiTypescript className="text-blue-700" size={30} /> },
-  { name: "MongoDB", icon: <SiMongodb className="text-[#00ED64]" size={30} /> },
-];
+  { name: 'React', icon: <FaReact className="text-blue-500" size={30} /> },
+  {
+    name: 'JavaScript',
+    icon: <SiJavascript className="text-yellow-500" size={30} />,
+  },
+  {
+    name: 'Next.js',
+    icon: <RiNextjsLine className="text-gray-800" size={30} />,
+  },
+  {
+    name: 'Styled-Components',
+    icon: <SiStyledcomponents className="text-pink-500" size={30} />,
+  },
+  { name: 'Node.js', icon: <FaNode className="text-green-500" size={30} /> },
+  {
+    name: 'TypeScript',
+    icon: <SiTypescript className="text-blue-700" size={30} />,
+  },
+  { name: 'MongoDB', icon: <SiMongodb className="text-[#00ED64]" size={30} /> },
+  {
+    name: 'Redux',
+    icon: <TbBrandRedux className="text-[#764abc]" size={30} />,
+  },
+]
 
 export const SectionRight = () => {
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-2xl font-semibold mb-4">Tecnologias que utilizo:</h2>
-      <div className="flex flex-wrap gap-3">
+      <div className="flex flex-wrap gap-3 justify-center">
         {technologies.map((tech, index) => (
           <div
             key={index}
@@ -31,7 +48,8 @@ export const SectionRight = () => {
       </div>
       <Card className="mt-6 w-full shadow-md">
         <CardContent className="pt-6">
-          Acesse as outras páginas para explorar mais sobre meu trabalho e projetos.
+          Acesse as outras páginas para explorar mais sobre meu trabalho e
+          projetos.
         </CardContent>
       </Card>
     </div>
